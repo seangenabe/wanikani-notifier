02bc2dbcd9cccad050479ca04f92c838d11c2cb9
Sean Genabe <seangenabe@outlook.com>
2015-06-14 22:50:48 +0800

    Added some features and colors here and there

---
+++ readme.md
@@ -0,0 +1,102 @@
+# wanikani-notifier
+
+Wanikani new lessons and reviews notifier
+
+## Usage
+
+### Install
+
+It is preferred to install this package globally.
+
+    npm i -g seangenabe/wanikani-notifier
+
+### API key
+
+To pass the API key, either pass it as an argument (start, install),
+set it in your npm config using `npm config wanikani-notifier:key`
+--this requires it to be launched from npm--, or
+set the `WANIKANI_NOTIFIER_API_KEY` environment variable.
+
+### CLI
+
+The notifier, when run, will query the WaniKani API for pending lessons and reviews.
+If there are no pending items, it will suspend until the next review time.
+
+`wanikani-notifier`
+`wanikani-notifier -h`
+`wanikani-notifier --help`
+
+Shows usage information.
+
+`wanikani-notifier start API_KEY`
+`npm config set wanikani-notifier API_KEY && npm start`
+
+Starts the notifier.
+
+Arguments:
+
+* `API_KEY`: Your WaniKani public API key. You can find this under Menu > Account.
+
+`wanikani-notifier install API_KEY`
+`npm config set wanikani-notifier API_KEY && npm run start:install`
+
+Installs the notifier to be called at startup. Requires global install.
+Currently Windows-only.
+
+Arguments:
+
+* `API_KEY`: Your WaniKani public API key. You can find this under Menu > Account.
+
+`wanikani-notifier uninstall`
+`npm run uninstall`
+
+Uninstalls the notifier from startup.
+
+### API
+
+`require('wanikani-notifier')` will return the WaniKaniNotifier class.
+
+`new WaniKaniNotifier(config)`
+
+Creates a new instance of the WaniKaniNotifier class.
+
+* `config.key`: Your WaniKani public API key. You can find this under Menu > Account.
+* `config.errorSuspendDuration`: How long to wait after encountering an error, in milliseconds. Default: 5 minutes
+* `config.notifiedSuspendDuration`: How long to wait after delivering a notification, in milliseconds. Default: 10 minutes
+* `config.waitingSuspendDuration`: At least how long to wait after determining that there are no pending items, in milliseconds. Default: 36 seconds
+* `config.dashboardOnBothPending`: Whether to go to the dashboard when there are both pending lessons and reviews. Default: false--goes to the lessons page.
+* `config.minilag`: A small amount of time to ensure pending items will be available the next time they will be queried, in milliseconds. Default: 1 second.
+
+`notifier.start()`
+
+Starts the notifier loop.
+
+Returns:
+* `Promise`: Resolved when the notifier loop ends (that is, after the notifier is stopped).
+
+`notifier.stop()`
+
+Stops the notifier loop.
+
+`notifier.process()`
+
+Represents a single run of the notifier loop. Queries the WaniKani API for any pending items.
+
+Returns:
+* `Promise`: Resolves with a `Number` representing the recommended number of milliseconds to suspend operation.
+
+`WaniKaniNotifier.console()`
+
+Runs the CLI. Internal use only, API usage not recommended as it makes assumptions about the environment
+(i.e. process.exit)
+
+`WaniKaniNotifier.getStartupPath()`
+
+Gets the path where the notifier installs the startup file.
+
+Returns:
+* `Promise`: Resolves with a `String` representing the path where the notifier installs the startup file.
+
+## License
+
+MIT
--- src/index.js
+++ src/index.js
@@ -1,35 +1,55 @@
 
-var Catbox = require('catbox')
-var CatboxMemory = require('catbox-memory')
 var Request = require('request')
 var Notifier = require('node-notifier')
 var Delayer = require('./delayer')
 var Moment = require('moment')
+var chalk = require('chalk')
+var open = require('open')
 
 class WaniKaniNotifier {
-  
+
   constructor(config) {
-    this.catbox = new Catbox.Client(CatboxMemory)
     config = config || {}
+    config.key = config.key || process.env.npm_package_config_key
     if (!config.key) {
       throw new Error("API key not specified.")
     }
-    config.errorSuspendDuration |= 1000 * 60 * 5
-    config.notifiedSuspendDuration |= 1000 * 60 * 10
-    config.waitingSuspendDuration |= 1000 * 36
+    config.errorSuspendDuration = config.errorSuspendDuration || 1000 * 60 * 5
+    config.notifiedSuspendDuration = config.notifiedSuspendDuration || 1000 * 60 * 10
+    config.waitingSuspendDuration = config.waitingSuspendDuration || 1000 * 36
+    config.dashboardOnBothPending = config.dashboardOnBothPending || false
+    config.minilag = config.minilag || 1000
     this.config = config
     this.lastNotification = {lessons: 0, reviews: 0}
+
+    Notifier.on('click', () => {
+      console.log('Notification clicked.')
+      var lessons = this.lastNotification.lessons
+      var reviews = this.lastNotification.reviews
+      var link
+      if (lessons && reviews) {
+        if (this.config.dashboardOnBothPending) {
+          link = 'https://www.wanikani.com/dashboard'
+        }
+        else {
+          link = 'https://www.wanikani.com/lesson'
+        }
+      }
+      else if (lessons) {
+        link = 'https://www.wanikani.com/lesson'
+      }
+      else if (reviews) {
+        link = 'https://www.wanikani.com/review'
+      }
+      else {
+        return
+      }
+
+      console.log('Opening ' + link)
+      open(link)
+    })
   }
-  
-  initialize() {
-    return new Promise(function(resolve, reject) {
-      this.catbox.start(function(err) {
-        if (err) return reject(err)
-        resolve()
-      })
-    }) 
-  }
-  
+
   async start() {
     try {
       while (true) {
@@ -43,20 +63,20 @@ class WaniKaniNotifier {
       throw err
     }
   }
-  
+
   stop() {
     if (this.delayer) {
       this.delayer.cancel()
     }
   }
-  
-  // Checks for pending notifications. Returns a time span representing the suggested next time to check for updates. 
+
+  // Checks for pending notifications. Returns a time span representing the suggested next time to check for updates.
   async process() {
     // Check for new items.
     var uri = 'https://www.wanikani.com/api/v1.3/user/' + encodeURIComponent(this.config.key) + '/study-queue';
 
     function scheduleNextCheck(duration) {
-      console.log('Will check back in ' + Moment.duration(duration).humanize())
+      console.log(chalk.yellow("Will check back in " + Moment.duration(duration).humanize()))
       return duration
     }
 
@@ -75,15 +95,16 @@ class WaniKaniNotifier {
 
       var data = JSON.parse(body)
       if (data.error) {
+        console.log(chalk.red("API error occured. Please make sure the configuration is correct."))
         console.error("API error occured. Please make sure the configuration is correct.")
         throw new Error(data.error.message)
       }
 
       // Assuming server is ahead.
       var timeDifference = new Date(response.headers.date) - new Date()
-      console.log("Response time is " + timeDifference + " milliseconds " +
+      console.log(chalk.dim("Response time is " + Math.abs(timeDifference) + " milliseconds " +
         ((timeDifference < 0) ? "behind" : "ahead") +
-        " of local time.")
+        " of local time."))
 
       var lessons = data.requested_information.lessons_available
       var reviews = data.requested_information.reviews_available
@@ -97,15 +118,18 @@ class WaniKaniNotifier {
             messages.push(reviews + " pending reviews")
           }
           var message = "You have " + messages.join(" and ") + "."
-          Notifier.notify({
-            title: "WaniKani Notifier",
-            message: message
-          })
-          console.log("Notification sent.")
           this.lastNotification = {
             lessons: lessons,
             reviews: reviews
           }
+          Notifier.notify({
+            title: "WaniKani Notifier",
+            message: message,
+            sound: true,
+            wait: true
+          })
+          console.log(chalk.bold(message))
+          console.log("Notification sent.")
         }
         else {
           console.log("You haven't touched your items yet since the last notification.")
@@ -114,53 +138,117 @@ class WaniKaniNotifier {
       }
 
       var nextReview = data.requested_information.next_review_date * 1000;
-      var timeBeforeNextReview = nextReview - timeDifference - new Date()
-      console.log("No pending items. Your next review will be in " + Moment.duration(timeBeforeNextReview).humanize())
+      var timeBeforeNextReview = nextReview - timeDifference - new Date() + minilag
+      console.log(chalk.yellow("No pending items. Your next review will be in " +
+        Moment.duration(timeBeforeNextReview).humanize()))
       var nextDelay = Math.max(timeBeforeNextReview, this.config.waitingSuspendDuration)
       return scheduleNextCheck(nextDelay)
     }
     catch (err) {
+      console.log(chalk.red(err.message))
       console.error(err)
       return scheduleNextCheck(this.config.errorSuspendDuration)
     }
   }
-  
+
   static console() {
     var nomnom = require('nomnom')()
-    var os = require('os')
-    
-    nomnom.option('key', {
+    var FS = require('fs')
+    var Readline = require('readline')
+
+    var apiKeyOption = {
       abbr: 'k',
       help: "Your WaniKani public API key. You can find this under Menu > Account.",
       metavar: 'API_KEY',
-      position: 1,
-      required: true
-    })
-    
+      position: 1
+    }
+
+    async function promptForKey(key) {
+      if (key) return key
+      key = process.env.npm_package_config_key || process.env.WANIKANI_NOTIFIER_API_KEY
+      if (key) return key
+
+      var i = Readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      })
+
+      key = await new Promise(function(resolve) {
+        i.question(chalk.bold("API key: "), function(k) {
+          resolve(k)
+        })
+      })
+      i.close()
+      if (key) return key
+
+      throw new Error("API key not specified.")
+    }
+
     var start = nomnom.command('start')
     start.help("Starts the notifier.")
-    start.callback(function(opts) {
-      var notifier = new WaniKaniNotifier({
-        key: opts.key
-      })
-      notifier.start()
-    })
-    
-    var install = nomnom.command('install')
-    install.help("Installs the notifier to be called at startup. Requires global install. Currently Windows-only.")
-    install.callback(function(opts) {
-      var Path = require('path')
-      var FS = require('fs')
-      // Windows
-      if (os.platform() == 'win32' && process.env.APPDATA) {
-        var startup = Path.join(process.env.APPDATA, 'Microsoft/Windows/Start Menu/Programs/Startup')
-        FS.writeFileSync("wanikani-notifier.cmd", "wanikani-notifier start --key " + opts.key)
+    start.option('key', apiKeyOption)
+    start.callback(async function(opts) {
+      try {
+        var key = await promptForKey(opts.key)
+        var notifier = new WaniKaniNotifier({
+          key: key
+        })
+        notifier.start()
+      }
+      catch (err) {
+        console.error(err.message)
+        process.exit(1)
       }
     })
-    
+
+    var install = nomnom.command('install')
+    install.help("Installs the notifier to be called at startup. Requires global install. Currently Windows-only.")
+    install.option('key', apiKeyOption)
+    install.callback(async function(opts) {
+      var startupPath = await WaniKaniNotifier.getStartupPath()
+      var key
+      try {
+        key = await promptForKey(opts.key)
+      }
+      catch (err) {
+        console.log(chalk.red(err.message))
+        console.error(err.message)
+        process.exit(1)
+      }
+      FS.writeFileSync(startupPath, "wanikani-notifier start " + key)
+    })
+
+    var uninstall = nomnom.command('uninstall')
+    uninstall.help("Uninstalls the notifier from startup.")
+    uninstall.callback(async function(opts) {
+      var startupPath = await WaniKaniNotifier.getStartupPath()
+      FS.unlinkSync(startupPath)
+    })
+
     nomnom.parse()
   }
-  
+
+  static async getStartupPath() {
+    var OS = require('os')
+    var Path = require('path')
+    var ChildProcess = require('child_process')
+    // Windows
+    if (OS.platform() === 'win32') {
+      var regQueryOutput = await new Promise(function(resolve, reject) {
+        ChildProcess.exec('reg query "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders" ' +
+        '/v Startup', function(error, stdout, stderr) {
+          if (error) {
+            reject(error)
+            return
+          }
+          resolve(stdout)
+        })
+      })
+      var startup = regQueryOutput.split(OS.EOL)[2].match(/\s+Startup\s+REG_SZ\s+(.*)/)[1]
+      return Path.join(startup, 'wanikani-notifier-startup.cmd')
+    }
+  }
+
 }
 
 module.exports = WaniKaniNotifier
